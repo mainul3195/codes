0ade82bacb6fd052b2b3023ec046e0ef16719bb1
mainul3195 <mainul3195@gmail.com>
2024-05-09 16:37:02 +0600

    .

--- extra/Problem2/Generator.cpp
+++ extra/Problem2/Generator.cpp
@@ -6,23 +6,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     registerGen(argc, argv, 1);
-    int n = rnd.next(1, 100000);
-    cout << n << "\n";
-    vector<int> m(n, 1);
-    int tot = n;
-    int final = rnd.next(n, 200000);
-    while (tot < final)
+    int t = rnd.next(1, 100000);
+    cout << t << "\n";
+    for (int i = 0; i < t; i++)
     {
-        int ind = rnd.next(0, n - 1);
-        m[ind]++;
-        tot++;
-    }
-    for (int i = 0; i < n; i++)
-        cout << m[i] << " \n"[i == n - 1];
-    for (int i = 0; i < n; i++)
-    {
-        int x = rnd.next(-1000000000, 1000000000);
-        int y = rnd.next(-1000000000, 1000000000);
-        cout << x << " " << y << "\n";
+        int d = rnd.next(1, 1000000000);
+        int h = rnd.next(1, 1000000000);
+        int theta = rnd.next(0, 60);
+        cout << d << " " << h << " " << theta << "\n";
     }
 }
