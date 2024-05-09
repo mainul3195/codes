47d6a0a23b2d01dd648fad5d91001b79f29dfe21
mainul3195 <mainul3195@gmail.com>
2024-05-09 20:42:08 +0600

    .

--- extra/Problem2/Generator.cpp
+++ extra/Problem2/Generator.cpp
@@ -1,18 +1,36 @@
-#include "testlib.h"
-#include <bits/stdc++.h>
+// #include "testlib.h"
+// #include <bits/stdc++.h>
 
+// using namespace std;
+
+// int main(int argc, char *argv[])
+// {
+//     registerGen(argc, argv, 1);
+//     int t = rnd.next(1, 100000);
+//     cout << t << "\n";
+//     for (int i = 0; i < t; i++)
+//     {
+//         int d = rnd.next(1, 1000000000);
+//         int h = rnd.next(1, 1000000000);
+//         int theta = rnd.next(0, 60);
+//         cout << d << " " << h << " " << theta << "\n";
+//     }
+// }
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char *argv[])
+void solve()
 {
-    registerGen(argc, argv, 1);
-    int t = rnd.next(1, 100000);
-    cout << t << "\n";
-    for (int i = 0; i < t; i++)
-    {
-        int d = rnd.next(1, 1000000000);
-        int h = rnd.next(1, 1000000000);
-        int theta = rnd.next(0, 60);
-        cout << d << " " << h << " " << theta << "\n";
-    }
+    cout << 61 << "\n";
+    for(int i = 0; i<=60; i++)
+        cout << 100 << " " << 100 << " " << i << "\n";
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    solve(); 
+    return 0;
 }
