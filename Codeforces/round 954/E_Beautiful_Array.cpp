0e63a3aa3b536c3e0c72b1e15274cc92817747c8
mainul3195 <mainul3195@gmail.com>
2024-06-26 07:59:52 +0600

    .

---
+++ Codeforces/round 954/E_Beautiful_Array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &i : v)
+        cin >> i;
+    sort(v.begin(), v.end());
+    for (auto i : v)
+        cout << i << " ";
+    cout << "\n";
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
