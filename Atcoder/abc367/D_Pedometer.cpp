23e6a77fbfa4b44e6d98c9c95e619afbcb30f12c
mainul3195 <mainul3195@gmail.com>
2024-08-23 10:23:18 +0600

    .

--- Atcoder/abc367/D_Pedometer.cpp
+++ Atcoder/abc367/D_Pedometer.cpp
@@ -10,27 +10,27 @@ void solve()
         cin >> v[i];
     for (int i = 1; i < n; i++)
         v.push_back(v[i]);
-    for (int i = 2; i < v.size(); i++)
+    for (int i = 1; i < v.size(); i++)
         (v[i] += v[i - 1]) %= k;
-    for (auto i : v)
-        cout << i << " ";
-    cout << "\n";
+    // for (auto i : v)
+    //     cout << i << " ";
+    // cout << "\n";
     long long ans = 0;
     cnt = vector<long long>(k + 1, 0);
     for (int i = 0; i < n; i++)
         cnt[v[i]]++;
-    for (auto i : cnt)
-        cout << i << " ";
-    cout << "\n";
+    // for (auto i : cnt)
+    //     cout << i << " ";
+    // cout << "\n";
     for (int i = n; i < v.size(); i++)
     {
+        cnt[v[i - n]]--;
         ans += cnt[v[i]];
         cnt[v[i]]++;
-        cnt[v[i - n]]--;
-        cout << i << "->";
-        for (auto j : cnt)
-            cout << j << " ";
-        cout << "\n";
+        // cout << i << "->";
+        // for (auto j : cnt)
+        //     cout << j << " ";
+        // cout << "\n";
     }
     cout << ans << "\n";
     return;
