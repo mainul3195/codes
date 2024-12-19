042de4aa41a15076bec7e6d974e8d5ac94508cb8
mainul3195 <mainul3195@gmail.com>
2024-12-19 10:38:09 +0600

    .

--- tfc2 kuet/C_C.cpp
+++ tfc2 kuet/C_C.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
+#define max_val 20000000000000LL
 vector<vector<long long>> v;
 long long find_min(int l1, int r1, int l2, int r2)
 {
-    long long mn = 2000000000;
+    long long mn = max_val;
     for (int i = l1; i <= r1; i++)
         for (int j = l2; j <= r2; j++)
             mn = min(mn, v[i][j]);
@@ -18,41 +19,42 @@ void solve()
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
             cin >> v[i][j];
-    long long ans = v[n / 2][n / 2];
-
+    int mid = n / 2 + 1;
+    long long ans = v[mid][mid];
     // dui ta
-    long long mn1 = 2000000000, mn2 = 2000000000;
-    for (int i = 1; i < n / 2; i++)
-        mn1 = min(mn1, v[i][n / 2]);
-    for (int i = n / 2 + 1; i <= n; i++)
-        mn2 = min(mn2, v[i][n / 2]);
+    long long mn1 = max_val, mn2 = max_val;
+    for (int i = 1; i < mid; i++)
+        mn1 = min(mn1, v[i][mid]);
+    for (int i = mid + 1; i <= n; i++)
+        mn2 = min(mn2, v[i][mid]);
     ans = min(ans, mn1 + mn2);
-    mn1 = 2000000000, mn2 = 2000000000;
-    for (int i = 1; i < n / 2; i++)
-        mn1 = min(mn1, v[n / 2][i]);
-    for (int i = n / 2 + 1; i <= n; i++)
-        mn2 = min(mn2, v[n / 2][i]);
+    mn1 = max_val, mn2 = max_val;
+    for (int i = 1; i < mid; i++)
+        mn1 = min(mn1, v[mid][i]);
+    for (int i = mid + 1; i <= n; i++)
+        mn2 = min(mn2, v[mid][i]);
     ans = min(ans, mn1 + mn2);
-
     // tin ta
-    long long mn = 2000000000;
-    for (int i = 1; i < n / 2; i++)
-        mn = min(mn, v[i][n / 2]);
-    ans = min({ans, mn + find_min(n / 2, n, 1, n / 2 - 1) + find_min(n / 2, n, n / 2 + 1, n)});
-    mn = 2000000000;
-    for (int i = n / 2 + 1; i < n; i++)
-        mn = min(mn, v[i][n / 2]);
-    ans = min({ans, mn + find_min(1, n / 2 - 1, 1, n / 2 - 1) + find_min(1, n / 2 - 1, n / 2 + 1, n)});
+    long long mn = max_val;
+    for (int i = 1; i < mid; i++)
+        mn = min(mn, v[i][mid]);
+    ans = min({ans, mn + find_min(mid, n, 1, mid - 1) + find_min(mid, n, mid + 1, n)});
+    mn = max_val;
+    for (int i = mid + 1; i < n; i++)
+        mn = min(mn, v[i][mid]);
+    ans = min({ans, mn + find_min(1, mid, 1, mid - 1) + find_min(1, mid, mid + 1, n)});
 
-    mn = 2000000000;
-    for (int i = 1; i < n / 2; i++)
-        mn = min(mn, v[n/2][i]);
-    ans = min({ans, mn + find_min(1, n/2-1, n/2, n) + find_min(n / 2, n, n / 2 + 1, n)});
-    mn = 2000000000;
-    for (int i = n / 2 + 1; i < n; i++)
-        mn = min(mn, v[n/2][i]);
-    ans = min({ans, mn + find_min(1, n / 2 - 1, 1, n / 2 - 1) + find_min(1, n / 2 - 1, n / 2 + 1, n)});
+    mn = max_val;
+    for (int i = 1; i < mid; i++)
+        mn = min(mn, v[mid][i]);
+    ans = min({ans, mn + find_min(1, mid - 1, mid, n) + find_min(mid + 1, n, mid, n)});
+    mn = max_val;
+    for (int i = mid + 1; i < n; i++)
+        mn = min(mn, v[mid][i]);
+    ans = min({ans, mn + find_min(1, mid - 1, 1, mid) + find_min(mid + 1, n, 1, mid)});
 
+    ans = min({ans, find_min(1, mid - 1, 1, mid - 1) + find_min(mid + 1, n, 1, mid - 1) + find_min(1, mid - 1, mid + 1, n) + find_min(mid + 1, n, mid + 1, n)});
+    cout << ans << "\n";
     return;
 }
 int main()
