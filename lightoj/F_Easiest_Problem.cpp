0bff65f62b07ecf73b9a9ebc8114ec94215d8687
mainul3195 <mainul3195@gmail.com>
2025-01-29 11:38:53 +0600

    .

---
+++ lightoj/F_Easiest_Problem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a = 5, b = 5, c = 5;
+    if (a == b == c)
+        cout << "Ok\n";
+    else
+        cout << "NO\n";
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
